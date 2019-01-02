b8f1c3ba8fa5627d19c0f6476bbff10b8a3dbbd0
karoberts <cppwriter@yahoo.com>
2019-01-01 16:55:26 -0800

    solved 14-2

--- 14.py
+++ 14.py
@@ -5,21 +5,29 @@ salt = 'ngcjuoqr'
 #salt = 'abc'
 
 n = 0
+nstretch = 1 # part1
+nstretch = 2017 # part2
 
 pat1 = re.compile(r'([a-z0-9])\1\1')
 pat2 = re.compile(r'([a-z0-9])\1{4}')
 hashes = {}
 keys = []
 
+def gethash(n):
+    md5 = salt + str(n)
+    for i in range(0, nstretch):
+        md5 = hashlib.md5(bytearray(md5, 'utf-8')).hexdigest()
+    return md5
+
 while len(keys) < 80:
-    md5 = hashes[n] if n in hashes else hashlib.md5(bytearray(salt + str(n), 'utf-8')).hexdigest()
+    md5 = hashes[n] if n in hashes else gethash(n)
     m = pat1.search(md5)
     if m:
         for i in range(0, 1000):
             nmd5 = None
             ni = n + i + 1
             if ni not in hashes:
-                nmd5 = hashlib.md5(bytearray(salt + str(ni), 'utf-8')).hexdigest()
+                nmd5 = gethash(ni)
                 hashes[ni] = nmd5
             else:
                 nmd5 = hashes[ni]
@@ -32,4 +40,4 @@ while len(keys) < 80:
 
     n += 1
 
-print('part1', keys[63])
+print(keys[63])
