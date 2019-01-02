457c3d68403e8645315214242eccfb6aff22aa5d
karoberts <cppwriter@yahoo.com>
2019-01-01 18:24:51 -0800

    solved 16-1

---
+++ 16.py
@@ -0,0 +1,27 @@
+disk = 20
+d = '10000'
+
+disk = 272
+d = '10111011111001111'
+
+intab = "10"
+outtab = "01"
+trantab = str.maketrans(intab, outtab)
+
+while len(d) < disk:
+    a = d
+    b = d[::-1].translate(trantab)
+    d = a + '0' + b
+
+d = d[:disk]
+print(d)
+
+def checksum(d):
+    ck = ''
+    for i in range(0, len(d), 2):
+        ck += '1' if d[i] == d[i+1] else '0'
+    if len(ck) % 2 == 0:
+        return checksum(ck)
+    return ck
+
+print(checksum(d))
