70d98d39a63d6fd62bc3eb9bd44dd7fefeea0bc6
karoberts <cppwriter@yahoo.com>
2020-02-06 16:00:42 -0800

    day 3 rust

--- rust/src/day03.rs
+++ rust/src/day03.rs
@@ -1,4 +1,37 @@
+use super::utils;
+
+fn test(a:i32, b:i32, c:i32) -> bool {
+    let mut v = vec![a, b, c];
+    v.sort();
+    v[0] + v[1] > v[2]
+}
 
 pub fn _run() 
 {
+    let lines = utils::read_lines("../3.txt").unwrap();
+    let mut ct = 0;
+    let mut groupings : Vec<Vec<i32>> = vec!();
+    for line in lines.map(|l| l.unwrap()) {
+        let mut ns : Vec<i32> = line.split(' ')
+            .filter(|x| x.len() > 0)
+            .map(|x| x.trim().parse::<i32>().unwrap())
+            .collect();
+        groupings.push(ns.clone());
+        ns.sort();
+        if ns[0] + ns[1] > ns[2] {
+            ct += 1;
+        }
+    }
+    println!("day03-1: {}", ct);
+
+    ct = 0;
+    for i in (0..groupings.len()).step_by(3) {
+        for j in 0..3 {
+            if test(groupings[i][j], groupings[i + 1][j], groupings[i + 2][j]) {
+                ct += 1;
+            }
+        }
+    }
+
+    println!("day03-2: {}", ct);
 }
