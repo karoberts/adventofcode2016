72a625cecb380999c5f612fb17881cb601e68ae6
karoberts <cppwriter@yahoo.com>
2021-10-18 22:13:17 -0700

    optimized day 12

--- rust/src/day12.rs
+++ rust/src/day12.rs
@@ -4,9 +4,9 @@ use super::utils;
 struct ProgItem
 {
     op: String,
-    arg1: String,
+    arg1: Option<char>,
     arg1i: Option<i32>,
-    arg2: Option<String>,
+    arg2: Option<char>,
     arg2i: Option<i32>,
     line: String
 }
@@ -20,10 +20,9 @@ fn is_abcd(s: &String) -> bool
     }
 }
 
-fn run(program : &Vec<ProgItem>, regs: &mut utils::HashMapFnv<String, i32>)
+fn run(program : &Vec<ProgItem>, regs: &mut utils::HashMapFnv<char, i32>)
 {
     let mut ip : i32 = 0;
-    let mut last_d : i32 = -1;
     loop
     {
         if ip >= program.len() as i32 || ip < 0
@@ -35,22 +34,17 @@ fn run(program : &Vec<ProgItem>, regs: &mut utils::HashMapFnv<String, i32>)
         let pline = &program[ip as usize];
 
         match pline.op.as_str() {
-            "cpy" => regs.insert(pline.arg2.as_ref().unwrap().to_owned(), pline.arg1i.or_else(|| Some(regs[&pline.arg1])).unwrap()),
-            "inc" => regs.insert(pline.arg1.to_owned(), regs[&pline.arg1] + 1),
-            "dec" => regs.insert(pline.arg1.to_owned(), regs[&pline.arg1] - 1),
+            "cpy" => regs.insert(pline.arg2.unwrap(), pline.arg1i.or_else(|| Some(regs[&pline.arg1.unwrap()])).unwrap()),
+            "inc" => regs.insert(pline.arg1.unwrap(), regs[&pline.arg1.unwrap()] + 1),
+            "dec" => regs.insert(pline.arg1.unwrap(), regs[&pline.arg1.unwrap()] - 1),
             "jnz" => None,
             _ => panic!("unsupported op")
         };
 
         if pline.op == "jnz" {
-            if (pline.arg1i.is_some() && pline.arg1i.unwrap() != 0) || (regs[&pline.arg1] != 0) {
+            if (pline.arg1i.is_some() && pline.arg1i.unwrap() != 0) || (regs[&pline.arg1.unwrap()] != 0) {
                 ip += pline.arg2i.unwrap() - 1;
             }
-        }
-
-        if regs["d"] != last_d
-        {
-            last_d = regs["d"];
         }
 
         ip += 1;
@@ -68,20 +62,20 @@ pub fn _run()
         let m : Vec<&str> = line.split(' ').collect();
         let p : ProgItem = ProgItem {
             op: m[0].to_owned(), 
-            arg1: m[1].to_owned(), 
+            arg1: if is_abcd(&m[1].to_owned()) { Some(m[1].chars().nth(0).unwrap()) } else { None }, 
             arg1i: if is_abcd(&m[1].to_owned()) { None } else { Some(m[1].parse::<i32>().expect("")) },
-            arg2: if m.len() == 2 {None} else {Some(m[2].to_owned())},
+            arg2: if m.len() == 3 && is_abcd(&m[2].to_owned()) { Some(m[2].chars().nth(0).unwrap()) } else { None }, 
             arg2i: if m.len() == 2 || is_abcd(&m[2].to_owned()) { None } else { Some(m[2].parse::<i32>().expect("")) },
             line: line
         };
         program.push(p);
     }
 
-    let mut regs = hashmap!["a".to_owned() => 0, "b".to_owned() => 0, "c".to_owned() => 0, "d".to_owned() => 0];
+    let mut regs = hashmap!['a' => 0, 'b' => 0, 'c' => 0, 'd' => 0];
     run(&program, &mut regs);
-    println!("day12-1: {}", regs["a"]);
+    println!("day12-1: {}", regs[&'a']);
 
-    regs = hashmap!["a".to_owned() => 0, "b".to_owned() => 0, "c".to_owned() => 1, "d".to_owned() => 0];
+    let mut regs = hashmap!['a' => 0, 'b' => 0, 'c' => 1, 'd' => 0];
     run(&program, &mut regs);
-    println!("day12-2: {}", regs["a"]);
+    println!("day12-2: {}", regs[&'a']);
 }
