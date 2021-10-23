81e5c38cc3019f3bf36def1be904d108d7efeb8b
karoberts <cppwriter@yahoo.com>
2021-10-22 22:33:10 -0700

    day 18 rust

--- rust/src/day18.rs
+++ rust/src/day18.rs
@@ -1,4 +1,40 @@
+fn is_trap(prev_row : &Vec<bool>, i: usize, nlen: usize) -> bool
+{
+    let l = if i == 0 { false } else { prev_row[i-1] };
+    let r = if i == nlen-1 { false } else { prev_row[i+1] };
+
+    return ! (l == r);
+}
+
+fn run(nrows:i32) -> usize
+{
+    let row1 = "......^.^^.....^^^^^^^^^...^.^..^^.^^^..^.^..^.^^^.^^^^..^^.^.^.....^^^^^..^..^^^..^^.^.^..^^..^^^..";
+
+    let nlen = row1.len();
+    let mut row_data : Vec<bool> = row1.chars().map(|c| c == '^').collect();
+    let mut nsafe = row_data.iter().filter(|x| !*x).count();
+
+    let mut next_row_data = (0..nlen).map(|_| false).collect();
+    let lists : [&mut Vec<bool>; 2] = [&mut row_data, &mut next_row_data];
+    let mut lid = 0;
+
+    for _ in 0..nrows - 1 {
+        let nlid = (lid + 1) % 2;
+        for i in 0..nlen {
+            let n = is_trap(&lists[lid], i, nlen);
+            lists[nlid][i] = n;
+            if ! n {
+                nsafe += 1;
+            }
+        }
+        lid = nlid;
+    }
+
+    return nsafe;
+}
 
 pub fn _run() 
 {
+    println!("day18-1: {}", run(40));
+    println!("day18-2: {}", run(400_000));
 }
