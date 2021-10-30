46c8f1a591c2ac7241fd972988b69c4f0b078a67
karoberts <cppwriter@yahoo.com>
2021-10-29 21:45:24 -0700

    day 20 rust

--- rust/src/day20.rs
+++ rust/src/day20.rs
@@ -1,4 +1,73 @@
+use super::utils;
+
+fn findrange(ranges:&Vec<(i64, i64)>, ip:i64) -> Option<&(i64, i64)>
+{
+    for rg in ranges {
+        if rg.0 <= ip && rg.1 >= ip {
+            return Some(rg);
+        }
+    }
+    return None
+}
+
+fn findnext(ranges:&Vec<(i64, i64)>, ip:i64) -> Option<&(i64, i64)>
+{
+    for rg in ranges {
+        if rg.0 > ip {
+            return Some(rg);
+        }
+    }
+    return None;
+}
+
+const MAX_IP : i64 = 4294967295;
 
 pub fn _run() 
 {
+    let s = utils::run_timer_start(20, 1);
+
+    let lines = utils::read_lines("../20.txt").unwrap();
+    let mut ranges : Vec<(i64, i64)> = vec!();
+
+    for line in lines.map(|l| l.unwrap()) {
+        let sides : Vec<i64> = line.split('-').map(|x| x.parse::<i64>().unwrap()).collect();
+        ranges.push( (sides[0], sides[1]) );
+    }
+
+    ranges.sort_by(|a, b| {
+        if a.0 == b.0 { (a.1 - a.0).cmp(&(b.1 - b.0)) } else { a.0.cmp(&b.0) }
+    });
+
+    let mut ip : i64 = 0;
+    while ip <= MAX_IP {
+        let rg = findrange(&ranges, ip);
+        if rg.is_none() {
+            utils::run_timer_end(s, ip);
+            break
+        }
+        ip = rg.unwrap().1;
+        ip += 1;
+    }
+
+    let s = utils::run_timer_start(20, 2);
+
+    ip = 0;
+    let mut ct = 0;
+    while ip <= MAX_IP {
+        let rg = findrange(&ranges, ip);
+        if rg.is_none() {
+            let mut nrg = findnext(&ranges, ip);
+            if nrg.is_none() {
+                nrg = Some(&(MAX_IP + 1, MAX_IP + 1));
+            }
+            ct += nrg.unwrap().0 - ip;
+            ip = nrg.unwrap().1;
+        }
+        else {
+            ip = rg.unwrap().1;
+            ip += 1;
+        }
+    }
+
+    utils::run_timer_end(s, ct);
 }
